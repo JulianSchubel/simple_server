b36814f936ae53395775fed36fd607a8ce727876
Julian Schubel <schubeljulian@gmail.com>
2022-11-18 12:18:51 +0200

    Added threading

--- main.c
+++ main.c
@@ -7,7 +7,7 @@
 #include <string.h>     /* strncat() */
 
 /* THREADING */
-//#include <pthread.h>
+#include <pthread.h>
 //#include <sys/types.h>  /* predefined typedefs and structures: ip_addr_t, pthread_mutex_t etc. */
 
 /* UTILITY FUNCTION PROTOTYPES AND DEFINITIONS */
@@ -24,6 +24,9 @@
 #define RESOURCE_BUFFER_LENGTH 1024
 #define RESPONSE_BUFFER_LENGTH 2048
 
+/* Initializer mutex */
+pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+
 enum methods {
     GET = 0,
     POST,       //Create/Update when exact resource location not known. I.e. determined by server.
@@ -33,13 +36,16 @@ enum methods {
     NUM_METHODS
 };
 
-void handle_connection(int sfd);
+void *handle_connection(void *sfd);
 
 /* handle_connection: create a new thread to handle the incoming request */
-void handle_connection(int sfd) 
+void *handle_connection(void *sfd) 
 {
+    /* Acquire the mutex lock */
+    pthread_mutex_lock(&lock);
+
     char request[2048];
-    read(sfd, request, 2048);
+    read(*((int*)sfd), request, 2048);
     /* Get the request method */
     /* 6 is max method length + '\0'. I.e. "DELETE\0" */
     char * request_method = (char *)malloc(sizeof(char)*MAX_METHOD_LENGTH);
@@ -83,7 +89,7 @@ void handle_connection(int sfd)
     strncat(response, response_body, (size_t)data_length);
 
     /* Send the response back to the client */
-    send(sfd, response, sizeof(response), 0);
+    send(*((int*)sfd), response, sizeof(response), 0);
 
     /* DEALLOCATE HEAP MEMORY */
     /* deallocate response buffer */
@@ -92,9 +98,13 @@ void handle_connection(int sfd)
     free(request_method);
 
     /* Close client socket */
-    close(sfd);
-}
+    close(*((int*)sfd));
 
+    /* Release the mutex lock */
+    pthread_mutex_unlock(&lock);
+    
+    return NULL;
+}
 
 int main(int argc, char **argv)
 {
@@ -121,6 +131,7 @@ int main(int argc, char **argv)
     check_listen(listen_status);
 
     while(1) {
+        pthread_t thread;
         int client_socket;
         /* Client address variables: IPv4 max 16 byte string representation "255.255.255.255\0", IPv6 46 byte string representation (IPv6 is 39 byte max but with IPv4 tunneling can go up to 45 and then 1 byte for null terminator) "nnnn:nnnn:nnnn:nnnn:nnnn:nnnn:nnnn:nnnn\0" (just IPv6), "nnnn:nnnn:nnnn:nnnn:nnnn:nnnn:nnnn:nnnn:255.255.255.255\0" with IPv4 tunneling */
         char client_address_string[MAX_CLIENT_ADDRESS_LENGTH];
@@ -135,8 +146,9 @@ int main(int argc, char **argv)
         //Convert client address from binary to text
         inet_ntop(client_address.sin_family, get_in_addr_type((struct sockaddr*)&client_address), client_address_string, client_addr_string_len);
         printf("Received connection from: %s\n", client_address_string);
-        /* client socket closed in handle_connection */
-        handle_connection(client_socket);
+        /* Open new thread to handle client connetion: client socket closed in handle_connection */
+        pthread_create(&thread, NULL, handle_connection, &client_socket);
+        //handle_connection(client_socket);
     }
     close(sfd);
 }
